f42d7a75d0428f13ca3c8b9cc7232886d814ae35
vpatov <vasiapatov@gmail.com>
2017-05-19 03:34:42 -0400

    crawling/scraping pretty much complete.

--- scraping/crawler.py
+++ scraping/crawler.py
@@ -16,37 +16,57 @@ artists = [i.strip() for i in open('artists.txt','r').read().split('\n')]
 
 
 def artist_url(band_name,page_num):
-    if (page_num):
-        return base_url + '/tabs/' + band_name.strip().replace(' ','_') + '_tabs' + str(page_num) + '.htm'
-    else:
-        return base_url + '/tabs/' + band_name.strip().replace(' ', '_') + '_tabs.htm'
+    return base_url + '/tabs/' + band_name.strip().replace(' ','_') + '_tabs' + str(page_num) + '.htm'
 
 
 #
-# status,response = http.request(artist_url('All That Remains'))
-# print response
-tab_file = open('tab_links.txt','w')
+# status,response = http.request(artist_url('All That Remains',1))
+
+
 
 tab_links = []
-# html_source = open('all_that_remains_tabs.htm','r').read()
-# tab_links += scraper.get_song_urls(html_source)
+response = open('all_that_remains_tabs.htm','r').read()
+tab_links += scraper.get_song_urls(response)
 
-driver = webdriver.Chrome()
+
+exit()
+tab_links = []
+tab_file = open('tab_links.txt','w')
 for artist in artists:
     page_num = 0
     while (True):
-
-
-        driver.get(artist_url(artist,page_num))
-        # time.sleep(3) # Let the user actually see something!
-        html_source = driver.page_source
-        if (len(html_source) < 8000):
+        status, response = http.request(artist_url(artist,page_num))
+        if (len(response) < 8000):
             break
-        tab_links += scraper.get_song_urls(html_source)
+        tab_links += scraper.get_song_urls(response)
         for tab in tab_links:
-            tab_file.write(tab + '\n')
-        time.sleep(2) # Let the user actually see something!
+            tab_file.write(tab[0] +',' + tab[1] + '\n')
         print "wrote page",page_num,"of", artist
         page_num += 1
 
-driver.quit()
+
+
+
+
+
+## Perhaps I don't need this damn webdriver anyway.
+#
+# driver = webdriver.Chrome()
+# for artist in artists:
+#     page_num = 0
+#     while (True):
+#
+#
+#         driver.get(artist_url(artist,page_num))
+#         # time.sleep(3) # Let the user actually see something!
+#         html_source = driver.page_source
+#         if (len(html_source) < 8000):
+#             break
+#         tab_links += scraper.get_song_urls(html_source)
+#         for tab in tab_links:
+#             tab_file.write(tab + '\n')
+#         time.sleep(2) # Let the user actually see something!
+#         print "wrote page",page_num,"of", artist
+#         page_num += 1
+#
+# driver.quit()
--- scraping/scraper.py
+++ scraping/scraper.py
@@ -12,15 +12,27 @@ def get_song_urls(artist_source):
             largest_tbody = tbody
 
     for tr in largest_tbody.findAll('tr'):
-        rating_td = tr.find('span',{'class':'rating'})
-        if (not rating_td):
-            continue
-        link = tr.find('a').get('href')
         trs = tr.findAll('td')
         if (trs):
             typeoftab = trs[-1].text
-        if float(rating_td['title']) > 4.0 and typeoftab.strip() == 'Guitar Pro':
-            urls.append(link)
+        if (typeoftab.strip() != 'Guitar Pro'):
+            continue
+
+        ###TODO filter out the same songs that end with (ver 2) and (ver 3)... and take the  best one
+        ###TODO replace findAll('tbody') with ('table') when doing from http response
+
+        rating_td = tr.find('span',{'class':'rating'})
+        if (not rating_td):
+            continue
+        rating_count = tr.find('font',{'class':'upc'})
+        if (not rating_count):
+            continue
+        rating_count = int(rating_count)
+        link = tr.find('a').get('href')
+        song_name = tr.find('a').text
+
+        if float(rating_td['title']) >= 4.0:
+            urls.append((link,song_name))
 
 
     return urls
